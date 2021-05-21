e5754692e805f8145a2fb2eeda50b6add9e3a3ca
koki kanasaka <k.koki980701@gmail.com>
2021-05-21 14:32:20 +0900

    プロフィール編集機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,17 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_user_path, notice: "ユーザー情報を編集しました！"
+    else
+      render :edit
+    end
+  end
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
