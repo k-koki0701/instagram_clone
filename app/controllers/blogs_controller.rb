2f07f43b2f936cc7fecd5c56ca3c12bd85687d85
koki kanasaka <k.koki980701@gmail.com>
2021-05-21 11:15:22 +0900

    誤字修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,68 +1,49 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: %i[ show edit update destroy ]
-
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
   def index
     @blogs = Blog.all
   end
-
-  def show
-  end
-
   def new
     @blog = Blog.new
   end
-
-  def edit
-  end
-
   def create
-    @blog = current_user.blogs.build(blog_params)
-
+    @blog = Blog.new(blog_params)
+    @blog.user_id = current_user.id
     if params[:back]
       render :new
     else
-    respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: "Blog was successfully created." }
-        format.json { render :show, status: :created, location: @blog }
+        redirect_to blogs_path, notice: "ブログを作成しました！"
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
+        render :new
       end
     end
   end
-
+  def show
+  end
+  def edit
+  end
   def update
-    respond_to do |format|
-      if @blog.update(blog_params)
-        format.html { redirect_to @blog, notice: "Blog was successfully updated." }
-        format.json { render :show, status: :ok, location: @blog }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
-      end
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit
     end
   end
-
   def destroy
     @blog.destroy
-    respond_to do |format|
-      format.html { redirect_to blogs_url, notice: "Blog was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to blogs_path, notice:"ブログを削除しました！"
   end
-
   def confirm
-    @blog = current_user.blogs.build(blog_params)
+    @blog = Blog.new(blog_params)
+    @blog.user_id = current_user.id
     render :new if @blog.invalid?
   end
-
   private
-    def set_blog
-      @blog = Blog.find(params[:id])
-    end
-
-    def blog_params
-      params.require(:blog).permit(:content, :picture)
-    end
+  def blog_params
+    params.require(:blog).permit(:content)
+  end
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
 end
--- app/views/blogs/_blog.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! blog, :id, :content, :picture, :created_at, :updated_at
-json.url blog_url(blog, format: :json)
