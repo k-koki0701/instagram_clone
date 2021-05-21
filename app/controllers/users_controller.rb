4cbf6c3368635fd280af932e41144f529f80b071
koki kanasaka <k.koki980701@gmail.com>
2021-05-21 14:43:13 +0900

    プロフに画像登録できる機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,6 @@ class UsersController < ApplicationController
   end
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache)
   end
 end
