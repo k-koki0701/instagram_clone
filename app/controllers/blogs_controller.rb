55ad2b625b1048a36655de097f8b33ad19bbffce
koki kanasaka <k.koki980701@gmail.com>
2021-05-21 13:43:42 +0900

    画像投稿機能

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -42,7 +42,7 @@ class BlogsController < ApplicationController
   end
   private
   def blog_params
-    params.require(:blog).permit(:content)
+    params.require(:blog).permit(:content, :picture, :picture_cache)
   end
   def set_blog
     @blog = Blog.find(params[:id])
