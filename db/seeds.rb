5b4717b43e462229dfcb205d7c4abe8caa53940b
koki kanasaka <k.koki980701@gmail.com>
2021-05-21 15:06:33 +0900

    誤字修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 50.times do |n|
-  name = Faker::JapaneseMedia::OnePiece.name
+  name = Faker::JapaneseMedia::OnePiece.character
   email = Faker::Internet.email
   password = "password"
   User.create!(name: name,
